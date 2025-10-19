991095c7ee99890db074ff04754ea84843ac67c9
Harshprabha <mahi.nofishmode@gmail.com>
2025-10-19 12:33:14 +0530

    adding resources

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ new Chart(ctx, {
         labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
         datasets: [{
             label: 'Time',
-            data: [8, 6, 7, 6, 10, 8, 4],
+            data: [3, 1.4, 2.5, 4, 3, 2, 4],
             backgroundColor: '#1e293b',
             borderWidth: 3,
             borderRadius: 6,
